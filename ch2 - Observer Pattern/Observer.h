aa649db5279afdabbb8b519979d810fc50e6371b
mohamed El-Raghy <elraghy8@gmail.com>
2022-11-07 22:40:11 +0200

    magic of weather station app

--- ch2 - Observer Pattern/Observer.h
+++ ch2 - Observer Pattern/Observer.h
@@ -4,7 +4,7 @@
 class Observer {
     public:
         virtual ~Observer() = default;
-        virtual void update(double temp, double humidity, double perssure);
+        virtual void update(double temp, double humidity, double perssure) = 0;
 };
 
 #endif  /*#ifndef OBSERVER_H*/
